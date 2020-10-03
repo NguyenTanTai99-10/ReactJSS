b9c697b215f9683097462d02f4d1e2efa2476e40
NguyenTanTai99-10 <g.stars.killer.12a3@gmail.com>
2020-10-03 16:59:32 +0700

    first commit

---
+++ src/component/Titles.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+class Titles extends React.Component{
+    render(){
+      return(
+        <div>
+          
+          <h1>Weather</h1>
+          <p>
+              Chao Ban Den Voi .......
+          </p>
+ 
+        </div>
+      );
+    }
+  };
+
+export default Titles
+
---
+++ src/component/Weather.js
@@ -0,0 +1,14 @@
+import React from 'react'
+class Weather extends React.Component{
+    render(){
+      return(
+        <div>
+            <p>
+                123
+            </p>
+        </div>
+      );
+    }
+  };
+
+export default Weather
